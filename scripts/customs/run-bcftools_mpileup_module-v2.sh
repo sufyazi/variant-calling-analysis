edea83929dda40bcd5966ee9ad4f6fa915bc3e82
sufyazi <sufyazi@outlook.com>
2023-10-30 16:34:05 +0800

    update mpileup module

--- scripts/customs/run-bcftools_mpileup_module-v2.sh
+++ scripts/customs/run-bcftools_mpileup_module-v2.sh
@@ -10,7 +10,7 @@ data_id=$4
 echo "TF motif of interest: $motif_id"
 
 # check if output vcf file already exists
-if [ -f "${outfile}"/"${data_id}"/"${data_id}"_"${motif_id}"_qualgt10.var.flt_VAF.vcf ]; then
+if [ -f "${outfile}"/"${data_id}"/"${data_id}"_"${motif_id}"_qualgt10.var.flt_VAF_new-csns.vcf ]; then
     echo "Output vcf file for ${data_id} already exists. Skipping..."
 else
     echo "Output vcf file for ${data_id} does not exist. Proceeding..."
@@ -21,7 +21,7 @@ else
         echo "Bed file path: $bed_path"
         mkdir -p "${outfile}"/"${data_id}"
         # call and filter variants from bam files of BRCA datasets
-        if bcftools mpileup -Ou -f /scratch/users/ntu/suffiazi/inputs/references/gatk4/GRCh38_no_alt_analysis_set_GCA_000001405.15.fasta -T "${bed_path}" -b "${bams}" --annotate FORMAT/AD,FORMAT/DP | bcftools call -Ou -mv | bcftools filter -i'QUAL>10' | bcftools +fill-tags - -- -t AF,VAF > "${outfile}"/"${data_id}"/"${data_id}"_"${motif_id}"_qualgt10.var.flt_VAF.vcf; then
+        if bcftools mpileup -Ou -f /scratch/users/ntu/suffiazi/inputs/references/gatk4/GRCh38_no_alt_analysis_set_GCA_000001405.15.fasta -T "${bed_path}" -b "${bams}" --annotate FORMAT/AD,FORMAT/DP | bcftools call -Ou -mv | bcftools filter -i'QUAL>10' | bcftools +fill-tags - -- -t AF,VAF > "${outfile}"/"${data_id}"/"${data_id}"_"${motif_id}"_qualgt10.var.flt_VAF_new-csns.vcf; then
                 echo "Variant calling and filtering completed."
             else
                 echo "Variant calling and filtering failed."
